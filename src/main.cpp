e049cfd704cae4b5a112ae5fba4753c3b87d57ad
Ben Carleton <ben.carleton@outlook.com>
2026-01-17 11:05:01 +0000

    Setup basic render loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,56 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main() {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	// glfwWindowHint(GLFW_OPENGL_FORWARFD_COMPAT, GL_TRUE);
+#include <iostream>
 
-	return 0;
+const unsigned int SRC_WIDTH = 800;
+const unsigned int SRC_HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  // Set the viewport dimensions
+  glViewport(0, 0, SRC_WIDTH, SRC_HEIGHT);
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+int main() {
+  // Initialise and configure glfw
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  // Create a window object
+  GLFWwindow *window = glfwCreateWindow(SRC_WIDTH, SRC_HEIGHT, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  // register callbacks
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // GLAD manages function ptrs for OpenGL, load OpenGL function ptrs for MacOS
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  // render loop
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
