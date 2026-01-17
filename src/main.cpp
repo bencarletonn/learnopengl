15473c362edb24c0cfeccdf579bf576cd52cc8ed
Ben Carleton <ben.carleton@outlook.com>
2026-01-17 11:57:50 +0000

    Add basic vertex shader

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,19 @@ void processInput(GLFWwindow *window) {
   }
 }
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+float vertices[] = {
+  -0.5f, -0.5f, 0.0f,
+   0.5f, -0.5f, 0.0f,
+   0.0f,  0.5f, 0.0f,
+};
+
 int main() {
   // Initialise and configure glfw
   glfwInit();
@@ -43,6 +56,21 @@ int main() {
     return -1;
   }
 
+  // vertex shader
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
+
   // render loop
   while (!glfwWindowShouldClose(window)) {
     // input
@@ -51,6 +79,11 @@ int main() {
     // render commands
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // check and call events, swap the bufferd
     glfwSwapBuffers(window);
