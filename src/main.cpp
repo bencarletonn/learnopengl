c2dbc84823ac9b6eb35477c874adf4513da6aa96
Ben Carleton <ben.carleton@outlook.com>
2026-01-31 15:33:23 +0000

    Apply translation before rotation - resulting in change of basis

--- src/main.cpp
+++ src/main.cpp
@@ -167,8 +167,8 @@ int main() {
 
     // create a translation matrix 
     glm::mat4 trans = glm::mat4(1.0f);
-    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
     trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
     unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
     glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
